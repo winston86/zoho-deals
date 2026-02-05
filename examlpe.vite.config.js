16c17c16e7731a9f1946965d8d6e425774faf7ad
winston86 <winston86.at.work@gmail.com>
2026-02-05 13:38:45 +0200

    Add dependencies

--- examlpe.vite.config.js
+++ examlpe.vite.config.js
@@ -1,13 +1,40 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue'; // <--- Import this
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js', '@zoho/app.js'],
             refresh: true,
         }),
-        vue(), // <--- Add this here
+    vue({ // 2. Initialize the plugin
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        tailwindcss(),
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js', // 3. Helpful alias for Blade templates
+            '@zoho': path.resolve(__dirname, 'vendor/winston86/zoho-deals/src/resources/js'),
+            'axios': path.resolve(__dirname, 'node_modules/axios'),
+        },
+    preserveSymlinks: true
+    },
+    server: {
+        watch: {
+            ignored: ['**/storage/framework/views/**'],
+        },
+    fs: {
+            // This allows Vite to serve files from the symlinked vendor directory
+            allow: ['..', './', '@zoho'] 
+        }
+    },
 });
